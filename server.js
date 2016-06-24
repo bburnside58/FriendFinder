14b10b07ccea02de70e5f7f8acf1ba16dd56389b
Brian Burnside <bcburnside58@gmail.com>
2016-06-24 00:15:39 -0400

    html done

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
 
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/app/public/home.html'));
+})
+
+app.get('/survey', function(req, res){	
+	res.sendFile(path.join(__dirname + '/app/public/survey.html'));
+})
 
 
 
